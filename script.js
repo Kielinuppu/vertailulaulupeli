cc3747bcbb6d95b85a1781ff9387f77bb66beab4
Kielinuppu <kielinuppu@gmail.com>
2024-11-11 10:41:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const sounds = {
     'aani1': new Audio('aani1.mp3'),
     'aani2': new Audio('aani2.mp3'),
     'aani3': new Audio('aani3.mp3'),
-    'aani4': new Audio('aani4.mp3'),
-    'aani5': new Audio('aani5.mp3'),
-    'aani6': new Audio('aani6.mp3')
+    'aani4': new Audio('aani4.mp3')
+    
 };
 
 let currentPlayingSound = null;
